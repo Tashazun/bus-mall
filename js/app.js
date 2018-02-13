c333fb67ef81cb63ef08a57c59018c2f8b116d72
Tasha R Zuniga <Tashazun@gmail.com>
2018-02-13 08:39:29 -0800

    Experiemented with ways to tally items shown

--- js/app.js
+++ js/app.js
@@ -3,26 +3,26 @@ const game = {
 
     start: function() {
         this.products.push(
-            new Product('imgs/bag.jpg','a'), 
-            new Product('imgs/banana.jpg','b'), 
-            new Product('imgs/bathroom.jpg','c'), 
-            new Product('imgs/boots.jpg','d'), 
-            new Product('imgs/breakfast.jpg','e'), 
-            new Product('imgs/bubblegum.jpg', 'f'), 
-            new Product('imgs/chair.jpg', 'g'), 
-            new Product('imgs/cthulhu.jpg', 'h'), 
-            new Product('imgs/dog-duck.jpg', 'i'), 
-            new Product('imgs/dragon.jpg', 'j'), 
-            new Product('imgs/pen.jpg', 'k'), 
-            new Product('imgs/pet-sweep.jpg', 'l'), 
-            new Product('imgs/scissors.jpg', 'm'), 
-            new Product('imgs/shark.jpg', 'n'), 
-            new Product('imgs/sweep.png', 'o'), 
-            new Product('imgs/tauntaun.jpg', 'p'), 
-            new Product('imgs/unicorn.jpg', 'q'), 
-            new Product('imgs/usb.gif', 'r'), 
-            new Product('imgs/water-can.jpg', 's'), 
-            new Product('imgs/wine-glass.jpg', 't') 
+            new Product('imgs/bag.jpg','bag'), 
+            new Product('imgs/banana.jpg','banana'), 
+            new Product('imgs/bathroom.jpg','bathroom'), 
+            new Product('imgs/boots.jpg','boots'), 
+            new Product('imgs/breakfast.jpg','breakfast'), 
+            new Product('imgs/bubblegum.jpg', 'bubblegum'), 
+            new Product('imgs/chair.jpg', 'chair'), 
+            new Product('imgs/cthulhu.jpg', 'cthulhu'), 
+            new Product('imgs/dog-duck.jpg', 'dog-duck'), 
+            new Product('imgs/dragon.jpg', 'dragon meat'), 
+            new Product('imgs/pen.jpg', 'pen'), 
+            new Product('imgs/pet-sweep.jpg', 'pet-sweep'), 
+            new Product('imgs/scissors.jpg', 'scissors'), 
+            new Product('imgs/shark.jpg', 'shark'), 
+            new Product('imgs/sweep.png', 'sweep'), 
+            new Product('imgs/tauntaun.jpg', 'tauntaun'), 
+            new Product('imgs/unicorn.jpg', 'unicorn meat'), 
+            new Product('imgs/usb.gif', 'tentacle'), 
+            new Product('imgs/water-can.jpg', 'water-can'), 
+            new Product('imgs/wine-glass.jpg', 'wine-glass') 
         );
 
     this.showPics();
@@ -44,21 +44,26 @@ const game = {
         game.showPics();
     });
 },
-    getRandomImg: function() {
-    const picSelection = [];
+   
+getRandomImg: function() {
+    picSelection = [];
     for (i= 0; picSelection.length < 3; i++) {
         const randomNumber = Math.floor(Math.random() * this.products.length);
         const selected = this.products[randomNumber];
     if (picSelection.indexOf(selected) === -1) {
         picSelection.push(selected);
-    } 
-        picSelection.forEach(function() {
-            selected.itemShown++;
-        })
+        } else if (picSelection.length == 3) {
+            picSelection.forEach(function() {
+                selected.itemShown++;
+            })
+
+        }
+
     };
     console.table(picSelection);
     return picSelection;
 },
+
 
     showPics: function() {
         const images = this.getRandomImg();
